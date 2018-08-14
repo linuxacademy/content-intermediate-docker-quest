536c075c74942c32d645d8d6300d7a03f686de10
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-08-14 12:14:17 -0500

    weather app

---
+++ config.js
@@ -0,0 +1,6 @@
+apiKey = '410bba55acb8ca522a483eb47758c68f';
+url = `http://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial`
+
+module.exports = {
+  'url': url
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {weather: null, error: null});
+});
+
+router.post('/', function(req, res, next) {
+  let city = req.body.city;
+  let url = config.url + `&q=${city}`;
+  console.log(url);
+
+  request(url, function(err, response, body) {
+    if(err) {
+      res.render('index', {weather: null, error: 'Error, please try again!'});
+    } else {
+      let weather = JSON.parse(body);
+
+      if(weather.main == undefined) {
+        res.render('index', {weather: null, error: 'Error, please try again!'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 });
 
 module.exports = router;
