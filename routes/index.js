c3b67137c59384f1059eefbb4bec6a2ee08feabb
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2019-01-28 13:46:58 -0600

    Add more console logging

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,17 @@ router.post('/', function(req, res, next) {
 
   request(url, function(err, response, body) {
     if(err) {
+      console.error('There was an error trying to lookup weather data for ' + city + '.');
       res.render('index', {weather: null, error: 'Error, please try again!'});
     } else {
       let weather = JSON.parse(body);
 
       if(weather.main == undefined) {
+        console.error('No weather data was avalable for ' + city + '.');
         res.render('index', {weather: null, error: 'Error, please try again!'});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        console.log("Found weather data for " + city + '.');
         res.render('index', {weather: weatherText, error: null});
       }
     }
